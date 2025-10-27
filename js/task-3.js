e1d260ba5cd71306959e65ccb122d21f2541a145
hiper <ivan.yuschuk77@gmail.com>
2025-10-27 12:59:44 +0200

    task 3 added

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,9 @@
 const input = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 input.addEventListener("input", (event) => {
-    output.textContent = event.currentTarget.value;
+    if (event.currentTarget.value.trim() === "") {
+        output.textContent = "Anonymous";
+        return;
+    }
+    output.textContent = event.currentTarget.value.trim();
 });
